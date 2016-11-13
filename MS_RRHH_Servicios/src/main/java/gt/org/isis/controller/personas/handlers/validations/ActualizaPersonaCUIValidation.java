a0a9f777ae0bbda3ba38a08f29196a86e5726ddb
edcracken <eliudiaz@gmail.com>
2016-11-13 14:19:08 -0600

    persona validations

---
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/validations/ActualizaPersonaCUIValidation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.org.isis.controller.personas.handlers.validations;
+
+import gt.org.isis.controller.dto.ReqNuevaPersonaDto;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author eliud
+ */
+@Component
+public class ActualizaPersonaCUIValidation extends CUIPersonaValidation<ReqNuevaPersonaDto> {
+
+}
