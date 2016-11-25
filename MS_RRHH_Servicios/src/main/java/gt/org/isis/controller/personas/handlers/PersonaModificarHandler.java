36942256683134e8bb9af2d0e0ad72bea0b6c6e5
edcracken <eliudiaz@gmail.com>
2016-11-25 11:33:04 -0600

    base class for dpi reader payload

--- MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/PersonaModificarHandler.java
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/PersonaModificarHandler.java
@@ -7,12 +7,6 @@ package gt.org.isis.controller.personas.handlers;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
-import gt.org.isis.api.AbstractValidationsRequestHandler;
-import gt.org.isis.api.C;
-import static gt.org.isis.api.ValidationsHelper.containsAny;
-import static gt.org.isis.api.ValidationsHelper.isNull;
-import gt.org.isis.api.misc.exceptions.ExceptionsManager;
-import gt.org.isis.controller.dto.DpiDto;
 import gt.org.isis.controller.dto.EstudioSaludDto;
 import gt.org.isis.controller.dto.IdiomaDto;
 import gt.org.isis.controller.dto.PersonaDto;
@@ -23,10 +17,6 @@ import gt.org.isis.converters.EstudiosSaludConverter;
 import gt.org.isis.converters.IdiomaDtoConverter;
 import gt.org.isis.converters.PersonaDtoConverter;
 import gt.org.isis.converters.RegistroLaboralPuestosConverter;
-import gt.org.isis.model.AreaGeografica;
-import gt.org.isis.model.AreaGeografica_;
-import gt.org.isis.model.Catalogos;
-import gt.org.isis.model.Catalogos_;
 import gt.org.isis.model.Dpi;
 import gt.org.isis.model.EstudioSalud;
 import gt.org.isis.model.HistoricoEstudioSalud;
@@ -43,9 +33,6 @@ import gt.org.isis.model.RegistroAcademico;
 import gt.org.isis.model.RegistroLaboral;
 import gt.org.isis.model.enums.EstadoVariable;
 import gt.org.isis.model.utils.EntitiesHelper;
-import static gt.org.isis.model.utils.EntitiesHelper.parseFechaDPI;
-import gt.org.isis.repository.AreasGeografRepository;
-import gt.org.isis.repository.CatalogosRepository;
 import gt.org.isis.repository.DpiRepository;
 import gt.org.isis.repository.EstudiosSaludHistoricoRepository;
 import gt.org.isis.repository.EstudiosSaludRepository;
@@ -57,19 +44,11 @@ import gt.org.isis.repository.PersonasRepository;
 import gt.org.isis.repository.PuestoRepository;
 import gt.org.isis.repository.RegistroAcademicoRepository;
 import gt.org.isis.repository.RegistroLaboralRepository;
-import java.util.Calendar;
 import java.util.List;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import org.joda.time.LocalDate;
-import org.joda.time.Years;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -84,7 +63,7 @@ import java.util.Collection;
  */
 @Service
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
-public class PersonaModificarHandler extends AbstractValidationsRequestHandler<ReqModPersonaDto, Boolean> {
+public class PersonaModificarHandler extends PersonasBaseHandler<ReqModPersonaDto, Boolean> {
 
     @Autowired
     PersonasRepository repo;
@@ -106,10 +85,6 @@ public class PersonaModificarHandler extends AbstractValidationsRequestHandler<R
     LugarResidenciaRepository registroLaboralRepo;
     @Autowired
     RegistroLaboralHistoricoRepository registroLabHistoricoRepo;
-    @Autowired
-    AreasGeografRepository areasRepo;
-    @Autowired
-    CatalogosRepository catalogosRepo;
     @Autowired
     PuestoRepository puestoRepo;
     @Autowired
@@ -174,55 +149,6 @@ public class PersonaModificarHandler extends AbstractValidationsRequestHandler<R
         repo.save(currentPersona);
 
         return this;
-    }
-
-    private void setDatosGeneralesByLector(Persona currentPersona, ReqModPersonaDto r) {
-        if (!isNull(r.getFechaNacimientoTexto()) && !r.getFechaNacimientoTexto().isEmpty()) {
-            currentPersona.setFechaNacimiento(parseFechaDPI(r.getFechaNacimientoTexto()));
-        } else {
-            throw ExceptionsManager.newValidationException("fecha_nacimiento", "fecha de nacimiento es requerida!");
-        }
-
-        currentPersona.setEdad(Years.yearsBetween(LocalDate.fromDateFields(currentPersona.getFechaNacimiento()),
-                LocalDate.fromDateFields(Calendar.getInstance().getTime())).getYears());
-
-        if (!isNull(r.getFkMunicipioNacimientoNombre()) && !r.getFkMunicipioNacimientoNombre().isEmpty()) {
-            currentPersona.setFkMunicipioNacimiento(getAreaByNombreAndTipo(r.getFkMunicipioNacimientoNombre(),
-                    C.CAT_AG_TIPO_MUNICIPIOS).getId());
-        } else {
-            throw ExceptionsManager.newValidationException("municipio_nacimiento", "municipio nacimiento es requerido!");
-        }
-
-        if (!isNull(r.getFkMunicipioCedulaNombre()) && !r.getFkMunicipioCedulaNombre().isEmpty()) {
-            currentPersona.setFkMunicipioCedula(getAreaByNombreAndTipo(r.getFkMunicipioCedulaNombre(),
-                    C.CAT_AG_TIPO_MUNICIPIOS).getId());
-        }
-
-        if (!isNull(r.getFkMunicipioVecindadNombre()) && !r.getFkMunicipioVecindadNombre().isEmpty()) {
-            currentPersona.setFkMunicipioVecindad(getAreaByNombreAndTipo(r.getFkMunicipioVecindadNombre(),
-                    C.CAT_AG_TIPO_MUNICIPIOS).getId());
-        } else {
-            throw ExceptionsManager.newValidationException("municipio_vecindad", "Municipio vecindad es requerido!");
-        }
-        if (!isNull(r.getFkNacionalidadNombre()) && !r.getFkNacionalidadNombre().isEmpty()) {
-            currentPersona.setFkNacionalidad(getCatalogoByNombreAndTipo(r.getFkNacionalidadNombre(),
-                    C.CAT_GEN_NACIONALIDAD).getId());
-        } else {
-            throw ExceptionsManager.newValidationException("municipio_nacimiento", "Nacionalidad es requerido!");
-        }
-        DpiDto dpiDto;
-        if ((dpiDto = r.getDpi()) != null) {
-            if (!isNull(dpiDto.getFechaEmisionTexto()) && !isNull(dpiDto.getFechaVencimientoTexto())) {
-                dpiDto.setFechaEmision(parseFechaDPI(dpiDto.getFechaEmisionTexto()));
-                dpiDto.setFechaVencimiento(parseFechaDPI(dpiDto.getFechaVencimientoTexto()));
-            } else if (isNull(dpiDto.getFechaVencimiento()) || isNull(dpiDto.getFechaEmision())) {
-                throw ExceptionsManager.newValidationException("dpi_datos",
-                        new String[]{"fechas_dpi,Debe ingresar datos de fecha vencimiento y emision del DPI"});
-            }
-        } else {
-            throw ExceptionsManager.newValidationException("dpi",
-                    new String[]{"dpi,Lectura de DPI es invalida, datos del DPI son requeridos!"});
-        }
     }
 
     private PersonaModificarHandler actualizarEstudiosSalud(Persona p, PersonaDto r) {
@@ -342,35 +268,4 @@ public class PersonaModificarHandler extends AbstractValidationsRequestHandler<R
         }
         return this;
     }
-
-    private Catalogos getCatalogoByNombreAndTipo(final String nombre, final String tipo) {
-        List<Catalogos> all = catalogosRepo.findAll(new Specification<Catalogos>() {
-            @Override
-            public Predicate toPredicate(Root<Catalogos> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
-                return cb.equal(root.get(Catalogos_.tipo), tipo);
-            }
-        });
-        for (Catalogos ag : all) {
-            if (containsAny(ag.getValor(), nombre)) {
-                return ag;
-            }
-        }
-        return all.get(0);
-    }
-
-    private AreaGeografica getAreaByNombreAndTipo(final String nombre, final String tipo) {
-        List<AreaGeografica> all = areasRepo.findAll(new Specification<AreaGeografica>() {
-            @Override
-            public Predicate toPredicate(Root<AreaGeografica> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
-                return cb.equal(root.get(AreaGeografica_.tipo), tipo);
-            }
-        });
-        for (AreaGeografica ag : all) {
-            if (containsAny(ag.getValor(), nombre)) {
-                return ag;
-            }
-        }
-        return all.get(0);
-    }
-
 }
--- MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/PersonasBaseHandler.java
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/PersonasBaseHandler.java
@@ -6,7 +6,31 @@
 package gt.org.isis.controller.personas.handlers;
 
 import gt.org.isis.api.AbstractValidationsRequestHandler;
+import gt.org.isis.api.C;
+import static gt.org.isis.api.ValidationsHelper.containsAny;
+import static gt.org.isis.api.ValidationsHelper.isNull;
+import gt.org.isis.api.misc.exceptions.ExceptionsManager;
+import gt.org.isis.controller.dto.DpiDto;
 import gt.org.isis.controller.dto.PersonaDto;
+import gt.org.isis.controller.dto.ReqModPersonaDto;
+import gt.org.isis.model.AreaGeografica;
+import gt.org.isis.model.AreaGeografica_;
+import gt.org.isis.model.Catalogos;
+import gt.org.isis.model.Catalogos_;
+import gt.org.isis.model.Persona;
+import static gt.org.isis.model.utils.EntitiesHelper.parseFechaDPI;
+import gt.org.isis.repository.AreasGeografRepository;
+import gt.org.isis.repository.CatalogosRepository;
+import java.util.Calendar;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 
 /**
  *
@@ -16,5 +40,87 @@ public abstract class PersonasBaseHandler<T extends PersonaDto, Q> extends Abstr
 
     @Override
     public abstract Q execute(T request);
+    @Autowired
+    AreasGeografRepository areasRepo;
+    @Autowired
+    CatalogosRepository catalogosRepo;
 
+    private Catalogos getCatalogoByNombreAndTipo(final String nombre, final String tipo) {
+        List<Catalogos> all = catalogosRepo.findAll(new Specification<Catalogos>() {
+            @Override
+            public Predicate toPredicate(Root<Catalogos> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+                return cb.equal(root.get(Catalogos_.tipo), tipo);
+            }
+        });
+        for (Catalogos ag : all) {
+            if (containsAny(ag.getValor(), nombre)) {
+                return ag;
+            }
+        }
+        return all.get(0);
+    }
+
+    private AreaGeografica getAreaByNombreAndTipo(final String nombre, final String tipo) {
+        List<AreaGeografica> all = areasRepo.findAll(new Specification<AreaGeografica>() {
+            @Override
+            public Predicate toPredicate(Root<AreaGeografica> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+                return cb.equal(root.get(AreaGeografica_.tipo), tipo);
+            }
+        });
+        for (AreaGeografica ag : all) {
+            if (containsAny(ag.getValor(), nombre)) {
+                return ag;
+            }
+        }
+        return all.get(0);
+    }
+
+    public void setDatosGeneralesByLector(Persona currentPersona, ReqModPersonaDto r) {
+        if (!isNull(r.getFechaNacimientoTexto()) && !r.getFechaNacimientoTexto().isEmpty()) {
+            currentPersona.setFechaNacimiento(parseFechaDPI(r.getFechaNacimientoTexto()));
+        } else {
+            throw ExceptionsManager.newValidationException("fecha_nacimiento", "fecha de nacimiento es requerida!");
+        }
+
+        currentPersona.setEdad(Years.yearsBetween(LocalDate.fromDateFields(currentPersona.getFechaNacimiento()),
+                LocalDate.fromDateFields(Calendar.getInstance().getTime())).getYears());
+
+        if (!isNull(r.getFkMunicipioNacimientoNombre()) && !r.getFkMunicipioNacimientoNombre().isEmpty()) {
+            currentPersona.setFkMunicipioNacimiento(getAreaByNombreAndTipo(r.getFkMunicipioNacimientoNombre(),
+                    C.CAT_AG_TIPO_MUNICIPIOS).getId());
+        } else {
+            throw ExceptionsManager.newValidationException("municipio_nacimiento", "municipio nacimiento es requerido!");
+        }
+
+        if (!isNull(r.getFkMunicipioCedulaNombre()) && !r.getFkMunicipioCedulaNombre().isEmpty()) {
+            currentPersona.setFkMunicipioCedula(getAreaByNombreAndTipo(r.getFkMunicipioCedulaNombre(),
+                    C.CAT_AG_TIPO_MUNICIPIOS).getId());
+        }
+
+        if (!isNull(r.getFkMunicipioVecindadNombre()) && !r.getFkMunicipioVecindadNombre().isEmpty()) {
+            currentPersona.setFkMunicipioVecindad(getAreaByNombreAndTipo(r.getFkMunicipioVecindadNombre(),
+                    C.CAT_AG_TIPO_MUNICIPIOS).getId());
+        } else {
+            throw ExceptionsManager.newValidationException("municipio_vecindad", "Municipio vecindad es requerido!");
+        }
+        if (!isNull(r.getFkNacionalidadNombre()) && !r.getFkNacionalidadNombre().isEmpty()) {
+            currentPersona.setFkNacionalidad(getCatalogoByNombreAndTipo(r.getFkNacionalidadNombre(),
+                    C.CAT_GEN_NACIONALIDAD).getId());
+        } else {
+            throw ExceptionsManager.newValidationException("municipio_nacimiento", "Nacionalidad es requerido!");
+        }
+        DpiDto dpiDto;
+        if ((dpiDto = r.getDpi()) != null) {
+            if (!isNull(dpiDto.getFechaEmisionTexto()) && !isNull(dpiDto.getFechaVencimientoTexto())) {
+                dpiDto.setFechaEmision(parseFechaDPI(dpiDto.getFechaEmisionTexto()));
+                dpiDto.setFechaVencimiento(parseFechaDPI(dpiDto.getFechaVencimientoTexto()));
+            } else if (isNull(dpiDto.getFechaVencimiento()) || isNull(dpiDto.getFechaEmision())) {
+                throw ExceptionsManager.newValidationException("dpi_datos",
+                        new String[]{"fechas_dpi,Debe ingresar datos de fecha vencimiento y emision del DPI"});
+            }
+        } else {
+            throw ExceptionsManager.newValidationException("dpi",
+                    new String[]{"dpi,Lectura de DPI es invalida, datos del DPI son requeridos!"});
+        }
+    }
 }
