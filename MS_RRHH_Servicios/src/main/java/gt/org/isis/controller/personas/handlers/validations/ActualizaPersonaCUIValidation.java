07add93302b602910520c3c521927b2811f23fab
edcracken <eliudiaz@gmail.com>
2016-11-27 08:30:05 -0600

    author fixed

--- MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/validations/ActualizaPersonaCUIValidation.java
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/validations/ActualizaPersonaCUIValidation.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 /**
  *
- * @author eliud
+ * @author edcracken
  */
 @Component
 public class ActualizaPersonaCUIValidation extends CUIPersonaValidation<RequestCreatePersonaDto> {
