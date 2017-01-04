99bd5fb429ed608059796475f43113eaa93947ed
edcracken <eliudiaz@gmail.com>
2017-01-04 15:06:47 -0600

    crud generic handlers

---
+++ MS_RRHH_Servicios/src/main/java/gt/org/ms/api/jpa/DoSaveEntityHandler.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.org.ms.api.jpa;
+
+import gt.org.ms.api.requesting.AbstractRequestHandler;
+import static gt.org.ms.api.requesting.ValidationsHelper.isNull;
+import gt.org.ms.api.entities.DesactivableEntity;
+import gt.org.ms.api.global.exceptions.ExceptionsManager;
+import gt.org.ms.repository.CustomRepository;
+import java.io.Serializable;
+import java.util.List;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author edcracken
+ * @param <T>
+ */
+public class DoSaveEntityHandler<T extends DesactivableEntity>
+        extends AbstractRequestHandler<T, Boolean> {
+
+    private final JpaRepository<T, Serializable> repo;
+    private Specification<T> customSpec;
+
+    public DoSaveEntityHandler(JpaRepository<T, Serializable> repo) {
+        this.repo = repo;
+    }
+
+    public void setCustomSpec(Specification<T> customSpec) {
+        this.customSpec = customSpec;
+    }
+
+    @Override
+    public Boolean execute(T e) {
+        if (!isNull(e.getId())) {
+            RuntimeException duplicatedEntityEx
+                    = ExceptionsManager
+                            .newValidationException("entity_exists", "Ya existe una entidad con ese ID");
+            if (!isNull(customSpec) && repo instanceof CustomRepository) {
+                List<T> l = ((CustomRepository) repo).findAll(customSpec);
+                if (!isNull(l) && !l.isEmpty()) {
+                    throw duplicatedEntityEx;
+                }
+            } else {
+                e = repo.findOne(e.getId());
+            }
+        }
+        if (!isNull(e)) {
+            repo.save(e);
+        } else {
+            throw ExceptionsManager.newValidationException("wrong_content", "La esta vacia o nula!");
+        }
+        return true;
+    }
+
+}
